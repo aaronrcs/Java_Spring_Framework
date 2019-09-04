2945007b5bdf7a06039ce155002abd44f3f531c9
aaronrcs <aromer61@calstatela.edu>
2019-09-04 15:59:20 -0700

    Added test controller files

--- dependency_injection_demo/src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ dependency_injection_demo/src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -1,6 +1,6 @@
-package guru.springframework;
+package guru.springframework.didemo;
 
-import guru.springframework.controllers.MyController;
+import guru.springframework.didemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
--- dependency_injection_demo/src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
+++ dependency_injection_demo/src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -5,6 +5,7 @@ import guru.springframework.didemo.services.GreetingServiceImpl;
 
 /**
  * Created by jt on 5/24/17.
+ * Example of a class of what not to do
  */
 
 public class PropertyInjectedController {
