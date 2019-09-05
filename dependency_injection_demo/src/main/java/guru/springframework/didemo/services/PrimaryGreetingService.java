35dd61bccc1256839d3dda42621b2c5fc188bc59
aaronrcs <aromer61@calstatela.edu>
2019-09-04 19:32:06 -0700

    Using @Profile to set a service/bean to a default bean

--- dependency_injection_demo/src/main/java/guru/springframework/didemo/services/PrimaryGreetingService.java
+++ dependency_injection_demo/src/main/java/guru/springframework/didemo/services/PrimaryGreetingService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 //Using @Service will tell the Spring context to bring this class as a Spring Bean
 @Service
 @Primary
-@Profile("eng")
+//"default means we want this bean to be the default bean
+// It will also be active when theres other profiles are not active
+@Profile({"eng", "default"})
 public class PrimaryGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
